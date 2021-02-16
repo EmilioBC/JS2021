6836f3515eaa9917fdcf24249b16e6a0e0e13994
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-02-16 06:22:17 +0000

    JSONandAJAX

---
+++ JSONandAJAX/main.js
@@ -0,0 +1,16 @@
+var pets = [
+  
+  {
+    "name": "Pussy",
+    "species": "cat",
+    "favFood": "Tuna"
+  },
+  {
+    "name": "Barky",
+    "species": "Dog",
+    "favFood": "Bone"
+  }
+
+]
+
+pets[1].name
